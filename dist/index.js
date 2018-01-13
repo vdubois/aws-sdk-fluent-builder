2fc8e5a64b70d3b284253d1eae8fa8c3738b8d37
Vincent Dubois <dubois.vct@free.fr>
2018-01-13 11:47:59 +0100

    [FIX] Path to main file

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// DynamoDB
+__export(require("./src/builders/dynamodb/dynamo-db.builder"));
+__export(require("./src/repositories/dynamodb/dynamo-db.repository.implementation"));
+__export(require("./src/repositories/dynamodb/dynamo-db.repository.proxy"));
+// S3
+__export(require("./src/builders/s3/s3.builder"));
+__export(require("./src/builders/s3/configuration/s3-configuration.builder"));
+__export(require("./src/builders/s3/hosting/s3-hosting.builder"));
+__export(require("./src/builders/s3/storage/s3-storage.builder"));
+__export(require("./src/repositories/s3/s3-configuration.service"));
+__export(require("./src/repositories/s3/s3-hosting.service"));
+__export(require("./src/repositories/s3/s3-storage.service"));
+// SNS
+__export(require("./src/builders/sns/sns.builder"));
+__export(require("./src/repositories/sns/sns.implementation"));
+__export(require("./src/repositories/sns/sns.proxy"));
---
+++ index.ts
@@ -0,0 +1,21 @@
+// DynamoDB
+export * from './src/builders/dynamodb/dynamo-db.builder';
+export * from './src/models/dynamo-db-table-caracteristics.model';
+export * from './src/repositories/dynamodb/dynamo-db.repository.implementation';
+export * from './src/repositories/dynamodb/dynamo-db.repository.proxy';
+export * from './src/repositories/dynamodb/dynamo-db.repository';
+
+// S3
+export * from './src/builders/s3/s3.builder';
+export * from './src/builders/s3/configuration/s3-configuration.builder';
+export * from './src/builders/s3/hosting/s3-hosting.builder';
+export * from './src/builders/s3/storage/s3-storage.builder';
+export * from './src/repositories/s3/s3-configuration.service';
+export * from './src/repositories/s3/s3-hosting.service';
+export * from './src/repositories/s3/s3-storage.service';
+
+// SNS
+export * from './src/builders/sns/sns.builder';
+export * from './src/repositories/sns/sns.implementation';
+export * from './src/repositories/sns/sns.proxy';
+export * from './src/repositories/sns/sns';
